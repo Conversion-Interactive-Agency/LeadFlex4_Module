27f52106d82ee2e4f88e9c8fec2c0261bf1dff7c
Jeffrey Benusa <jbenusa@conversionia.com>
2023-09-25 10:53:05 -0500

    change section url pattern to {slug}-{id} instead of building slug via multiple save events

--- src/LeadFlex.php
+++ src/LeadFlex.php
@@ -3,24 +3,16 @@ namespace conversionia\leadflex;
 
 use Craft;
 
-use conversionia\leadflex\webhooks\DriverReachFormie;
-use conversionia\leadflex\webhooks\TenstreetFormie;
-use conversionia\leadflex\webhooks\EbeFormie;
-use conversionia\leadflex\webhooks\UkgFormie;
-use conversionia\leadflex\exporters\GeosheetExporter;
-
-use conversionia\leadflex\assets\ControlPanel;
-
-use craft\feedme\events\FeedProcessEvent;
-use craft\feedme\services\Process;
-
-use verbb\formie\events\RegisterIntegrationsEvent;
-use verbb\formie\services\Integrations;
-
 use yii\base\Event;
 use yii\base\Exception;
 use yii\base\Module;
 use yii\base\InvalidConfigException;
+
+use verbb\formie\events\RegisterIntegrationsEvent;
+use verbb\formie\services\Integrations;
+
+use craft\feedme\events\FeedProcessEvent;
+use craft\feedme\services\Process;
 
 use craft\errors\ElementNotFoundException;
 use craft\elements\Entry;
@@ -28,6 +20,7 @@ use craft\base\Element;
 use craft\events\ModelEvent;
 use craft\events\RegisterElementExportersEvent;
 use craft\helpers\StringHelper;
+use craft\helpers\ElementHelper;
 
 class LeadFlex extends Module
 {
@@ -52,46 +45,14 @@ class LeadFlex extends Module
         // Adjust controller namespace for console requests
         if ($request->getIsConsoleRequest()) {
             $this->controllerNamespace = 'conversionia\leadflex\console\controllers';
-            $this->_registerConsoleEventListeners();
         } else {
             if ($request->getIsCpRequest()) {
-                Craft::$app->view->registerAssetBundle(ControlPanel::class);
+                Craft::$app->view->registerAssetBundle(\conversionia\leadflex\assets\ControlPanel::class);
                 $this->_registerExporters();
             }
         }
-
         $this->_registerFormieIntegrations();
-        $this->_registerSaveEntryEvents();
-    }
-
-    private function _registerConsoleEventListeners()
-    {
-        Event::on(Process::class, Process::EVENT_STEP_BEFORE_PARSE_CONTENT, [$this, 'beforeParseContent']);
-    }
-
-    private function _registerSaveEntryEvents()
-    {
         Event::on(Entry::class, Element::EVENT_BEFORE_SAVE, [$this, 'entryBeforeSave']);
-        Event::on(Entry::class, Element::EVENT_AFTER_SAVE, [$this, 'entryAfterSave']);
-    }
-
-    /**
-     * @throws InvalidConfigException
-     * Removing the slug field from the feed mapping for the jobs section
-     */
-    public function beforeParseContent(FeedProcessEvent $event)
-    {
-        $entry = $event->element;
-        if(!$entry instanceof Entry){
-            return false;
-        }
-
-        $handle = strtolower($entry->section->handle);
-        if (isset($entry->id) && $handle == $this->key) {
-            unset($event->feed['fieldMapping']['title']);
-            unset($event->feed['fieldMapping']['slug']);
-            return $event;
-        }
     }
 
     function entryBeforeSave(ModelEvent $event)
@@ -103,34 +64,17 @@ class LeadFlex extends Module
         if (!$validated) {
             return;
         }
-
+        // Statewide field toggle
         $location = $entry->getFieldValue('location');
         $isStatewide = empty($location['city']);
         $event->sender->setFieldValue('statewideJob', $isStatewide);
-    }
 
-    /**
-     * @throws Exception
-     * @throws \Throwable
-     * @throws ElementNotFoundException
-     */
-    function entryAfterSave(ModelEvent $event)
-    {
-        $entry = $event->sender;
-        $handle = strtolower($entry->section->handle);
-        $validated = $handle === $this->key;
-
-        if ($validated) {
-            $defaultJob = $entry->getFieldValue('defaultJobDescription')->one();
-            $isProtected = $entry->getFieldValue('protectedSlug');
-            if (!empty($defaultJob) && !$isProtected) {
-                $titleText = !empty($entry->adHeadline) ? $entry->adHeadline
-                    : (!empty($defaultJob->adHeadline) ? $defaultJob->adHeadline : $defaultJob->title);
-                $title = StringHelper::slugify($titleText);
-                $entry->slug = $title . "-" . $entry->id;
-                $entry->setFieldValue('protectedSlug', true);
-                Craft::$app->elements->saveElement($entry);
-            }
+        // Slug Manipulation On first save
+        $defaultJob = $entry->getFieldValue('defaultJobDescription')->one();
+        if (!empty($defaultJob) && ElementHelper::isTempSlug($entry->slug)) {
+            $titleText = !empty($entry->adHeadline) ? $entry->adHeadline
+                : (!empty($defaultJob->adHeadline) ? $defaultJob->adHeadline : $defaultJob->title);
+            $entry->slug = StringHelper::slugify($titleText);
         }
     }
 
@@ -143,10 +87,10 @@ class LeadFlex extends Module
             Integrations::class,
             Integrations::EVENT_REGISTER_INTEGRATIONS,
             static function(RegisterIntegrationsEvent $event) {
-                $event->webhooks[] = TenstreetFormie::class;
-                $event->webhooks[] = DriverReachFormie::class;
-                $event->webhooks[] = EbeFormie::class;
-                $event->webhooks[] = UkgFormie::class;
+                $event->webhooks[] = \conversionia\leadflex\webhooks\DriverReachFormie::class;
+                $event->webhooks[] = \conversionia\leadflex\webhooks\TenstreetFormie::class;
+                $event->webhooks[] = \conversionia\leadflex\webhooks\EbeFormie::class;
+                $event->webhooks[] = \conversionia\leadflex\webhooks\UkgFormie::class;
             }
         );
     }
@@ -158,7 +102,7 @@ class LeadFlex extends Module
             Entry::class,
             Element::EVENT_REGISTER_EXPORTERS,
             static function (RegisterElementExportersEvent $event) {
-                $event->exporters[] = GeosheetExporter::class;
+                $event->exporters[] = \conversionia\leadflex\exporters\GeosheetExporter::class;
             }
         );
     }
