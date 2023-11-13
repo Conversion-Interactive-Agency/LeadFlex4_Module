c3d1161a2840671cef1a4095a99b9d71bc5b66dc
Jeffrey Benusa <jbenusa@conversionia.com>
2023-11-13 13:53:31 -0600

    Moving Event registrations into Services.

--- src/LeadFlex.php
+++ src/LeadFlex.php
@@ -3,31 +3,21 @@ namespace conversionia\leadflex;
 
 use Craft;
 
-use conversionia\leadflex\webhooks\DriverReachFormie;
-use conversionia\leadflex\webhooks\TenstreetFormie;
-use conversionia\leadflex\webhooks\EbeFormie;
-use conversionia\leadflex\webhooks\UkgFormie;
-use conversionia\leadflex\webhooks\StarsCampusFormie;
-use conversionia\leadflex\exporters\GeosheetExporter;
+use conversionia\leadflex\services\WebhooksService;
+use conversionia\leadflex\services\ExportsService;
+use conversionia\leadflex\services\EntryService;
+use conversionia\leadflex\services\FeedmeService;
+use conversionia\leadflex\services\ControlPanelService;
 
 use conversionia\leadflex\assets\ControlPanel;
 
-use craft\feedme\events\FeedProcessEvent;
-use craft\feedme\services\Process;
-
-use verbb\formie\events\RegisterIntegrationsEvent;
-use verbb\formie\services\Integrations;
-
-use yii\base\Event;
-use yii\base\Exception;
 use yii\base\Module;
-use yii\base\InvalidConfigException;
 
-use craft\errors\ElementNotFoundException;
+
 use craft\elements\Entry;
 use craft\base\Element;
 use craft\events\ModelEvent;
-use craft\events\RegisterElementExportersEvent;
+
 use craft\helpers\StringHelper;
 
 class LeadFlex extends Module
@@ -48,160 +38,28 @@ class LeadFlex extends Module
         // Set alias for this module
         Craft::setAlias('@conversionia', __DIR__);
 
-        $request = Craft::$app->getRequest();
+        // Register our services
+        $this->setComponents([
+            'controlpanel' => ControlPanel::class,
+            'entry' => EntryService::class,
+            'exports' => ExportsService::class,
+            'feedme' => FeedmeService::class,
+            'webhooks' => WebhooksService::class,
+        ]);
 
+        // Register Events
+        $request = Craft::$app->getRequest();
         // Adjust controller namespace for console requests
         if ($request->getIsConsoleRequest()) {
             $this->controllerNamespace = 'conversionia\leadflex\console\controllers';
-            $this->_registerConsoleEventListeners();
+            $this->entry->registerEvents();
+            $this->feedme->registerEvents();
         }
 
         if ($request->getIsCpRequest()) {
-            Craft::$app->view->registerAssetBundle(ControlPanel::class);
-            $this->_registerExporters();
+            $this->controlpanel->init();
+            $this->exports->registerEvents();
+            $this->webhooks->registerEvents();
         }
-
-        $this->_registerFormieIntegrations();
-        $this->_registerSaveEntryEvents();
-    }
-
-    private function _registerConsoleEventListeners()
-    {
-        Event::on(Process::class, Process::EVENT_STEP_BEFORE_PARSE_CONTENT, [$this, 'beforeParseContent']);
-    }
-
-    private function _registerSaveEntryEvents()
-    {
-        Event::on(Entry::class, Element::EVENT_BEFORE_SAVE, [$this, 'entryBeforeSave']);
-        Event::on(Entry::class, Element::EVENT_AFTER_SAVE, [$this, 'entryAfterSave']);
-    }
-
-    /**
-     * @throws InvalidConfigException
-     * Removing the slug field from the feed mapping for the jobs section
-     */
-    public function beforeParseContent(FeedProcessEvent $event)
-    {
-        $entry = $event->element;
-        if (!$this->isJobEntry($entry)) {
-            return false;
-        }
-        $isExistingElement = $entry->id;
-        if ($isExistingElement) {
-            unset($event->feed['fieldMapping']['title']);
-            unset($event->feed['fieldMapping']['slug']);
-            return $event;
-        }
-    }
-
-    function entryBeforeSave(ModelEvent $event)
-    {
-        $entry = $event->sender;
-        $fields = ['location','statewideJob','advertiseJob','assignedCampaign'];
-        if (!$this->doFieldsExists($entry, $fields)) {
-            return;
-        }
-
-        $assignedCampaign = $entry->getFieldValue('assignedCampaign')->one();
-        if(!$entry->enabled || is_null($assignedCampaign)){
-            $event->sender->setFieldValue('advertiseJob', 'false');
-            $event->sender->setFieldValue('assignedCampaign', []);
-        }
-
-        $location = $entry->getFieldValue('location');
-        $isStatewide = empty($location['city']);
-        $event->sender->setFieldValue('statewideJob', $isStatewide);
-    }
-
-    /**
-     * @throws Exception
-     * @throws \Throwable
-     * @throws ElementNotFoundException
-     */
-    function entryAfterSave(ModelEvent $event)
-    {
-        $entry = $event->sender;
-        $fields = ['protectedSlug','defaultJobDescription','protectedSlug'];
-        if (!$this->doFieldsExists($entry, $fields)) {
-            return;
-        }
-
-        $defaultJob = $entry->getFieldValue('defaultJobDescription')->one();
-        $isProtected = $entry->getFieldValue('protectedSlug');
-        if (!empty($defaultJob) && !$isProtected) {
-            $titleText = !empty($entry->adHeadline) ? $entry->adHeadline
-                : (!empty($defaultJob->adHeadline) ? $defaultJob->adHeadline : $defaultJob->title);
-            $title = StringHelper::slugify($titleText);
-            $entry->slug = $title . "-" . $entry->id;
-            $entry->setFieldValue('protectedSlug', true);
-            Craft::$app->elements->saveElement($entry);
-        }
-    }
-
-    /**
-     * Register custom webhook for Formie.
-     */
-    private function _registerFormieIntegrations()
-    {
-        Event::on(
-            Integrations::class,
-            Integrations::EVENT_REGISTER_INTEGRATIONS,
-            static function(RegisterIntegrationsEvent $event) {
-                $event->webhooks[] = TenstreetFormie::class;
-                $event->webhooks[] = DriverReachFormie::class;
-                $event->webhooks[] = EbeFormie::class;
-                $event->webhooks[] = UkgFormie::class;
-                $event->webhooks[] = StarsCampusFormie::class;
-            }
-        );
-    }
-
-    private function _registerExporters()
-    {
-        // Register exporters
-        Event::on(
-            Entry::class,
-            Element::EVENT_REGISTER_EXPORTERS,
-            static function (RegisterElementExportersEvent $event) {
-                $event->exporters[] = GeosheetExporter::class;
-            }
-        );
-    }
-
-    // Check if a field exists
-    private function doFieldsExists($entry, $fieldHandle): bool
-    {
-        if (!$this->isJobEntry($entry)) {
-            return false;
-        }
-
-        $hasAllFields = true;
-
-        if (!is_array($fieldHandle)){
-            $fieldHandle = [$fieldHandle];
-        }
-
-        $entryFields = $entry->getType()->getFieldLayout()->getFields();
-
-        // transform the array of Field objects into an array of field handles for convenience
-        $entryFieldHandles = array_column($entryFields, 'handle');
-
-        // check entry has fields
-        foreach ($fieldHandle as $handle) {
-            $entryHasMyCustomField = in_array($handle, $entryFieldHandles);
-            if (!$entryHasMyCustomField) {
-                $hasAllFields = false;
-            }
-        }
-
-        return $hasAllFields;
-    }
-
-    private function isJobEntry($entry):bool
-    {
-        if(!$entry instanceof Entry){
-            return false;
-        }
-        return $this->section == $entry->section->handle;
     }
 }
--- src/webhooks/TalentDriverFormie.php
+++ src/webhooks/TalentDriverFormie.php
@@ -2,23 +2,30 @@
 namespace conversionia\leadflex\webhooks;
 
 use Craft;
+use craft\base\Volume;
 use verbb\formie\elements\Form;
 use verbb\formie\elements\Submission;
 use verbb\formie\Formie;
 use verbb\formie\integrations\webhooks\Webhook;
 
-class EbeFormie extends Webhook
+// Volume Types
+use craft\base\LocalVolumeInterface;
+
+// todo: Build postFowarinding from Digital Ocean;
+// use vaersaagod\dospaces\Volume as DigitalOceanVolume;
+
+class TalentDriverFormie extends Webhook
 {
     public $webhook;
 
     public static function displayName(): string
     {
-        return Craft::t('formie', 'EBE ATS');
+        return Craft::t('formie', 'Talent Driver');
     }
 
     public function getDescription(): string
     {
-        return Craft::t('formie', 'This is a EBE ATS webhook integration.');
+        return Craft::t('formie', 'Talent Driver webhook integration.');
     }
 
     public function getIconUrl(): string
@@ -48,18 +55,37 @@ class EbeFormie extends Webhook
     {
         /** @var Form $form */
         $form = $submission->getForm();
+        $fields = $form->getFields();
 
         // Initialize form data
         $data = [];
         $labels = [];
 
-        // Get every submitted field value
-        foreach ($form->getFields() as $field) {
+        $requiredFields = [];
+        // Data has already been assigned
 
+        $requiredFieldsMapping = [
+            'firstName' => 'given_name',
+            'lastName' => 'family_name',
+            'cellPhone' => 'phone',
+            'email' => 'email',
+            'birthDate' => 'birth_date',
+            'city' => 'city',
+            'state' => 'state',
+            'zipCode' => 'zip',
+            'optIn' => 'sms_consent',
+            'experience' => 'experience',
+            'accidents' => 'accidents',
+            'violations' => 'violations',
+            'referrerValue' => 'source',
+        ];
+
+        // Get every submitted field value
+        foreach ($fields as $field) {
             // Get data
             $value = $submission->getFieldValue($field->handle);
-            $data[$field->handle] = $field->getValueAsString($value, $submission);
-
+            $data[$field->handle] = trim($field->getValueAsString($value, $submission));
+            unset($requiredFieldsMapping[$field->handle]);
             // Get labels
             $label = $field->getAttributeLabel($field->handle);
             $labels[$field->handle] = $label;
@@ -68,8 +94,6 @@ class EbeFormie extends Webhook
             $useDefaults = [
                 'atsCompanyId',
                 'companyName',
-                'source',
-                'sourceId',
             ];
 
             // Fallback to default value if necessary
@@ -84,51 +108,57 @@ class EbeFormie extends Webhook
         // Get driver ID
         $driverId = $data['webPageUrl'].'?driverId='.$submission->id;
 
-        // Get license class
-        // $licenseClass = ('Yes' === $data['cdlA'] ? 'CDL-A' : '');
+        // Check conditions if should be sent to testing environment
+        $setAsTestSubmission =
+            $data['firstName'] == $data['lastName'] ||
+            $data['firstName'] == 'test' ||
+            $data['lastName'] == 'test';
+        $company = !$setAsTestSubmission ? $data['companyName'] : 'test';
 
         // Compile JSON data
         $json = [
-            'form' => [
-                'id' => $form->id,
-                'name' => $form->title,
-                'returnUrl' => $form->getRedirectUrl(),
-                'CompanyName' => trim($data['companyName']),
-                'CompanyId' => trim($data['atsCompanyId']),
-                'source' => trim($data['source']),
-                'sourceId' => trim($data['sourceId']),
-                'DriverId' => $driverId,
-                'AppReferrer' => trim($data['referrerValue']),
-                'GivenName' => trim($data['firstName']),
-                'FamilyName' => trim($data['lastName']),
-                'Municipality' => trim($data['city']),
-                'Region' => trim($data['state']),
-                'PostalCode' => trim($data['zipCode']),
-                'InternetEmailAddress' => trim($data['email']),
-                'PrimaryPhone' => $this->_cleanPhone($data['cellPhone']),
-                // 'CommercialDriversLicense' => $data['cdlA'],
-                // 'LicenseClass' => $licenseClass,
-                'OptIn' => ($data['optIn'] ?? null || 'No' ?: 'No' ),
+            'params' => [
+                'company' => $company
             ],
+            'headers'=>[],
+            'body' => [
+                "given_name"=>  trim($data['firstName']),
+                "family_name"=> trim($data['lastName']),
+                "phone"=> $this->_cleanPhone($data['cellPhone']),
+                "email"=> trim($data['email']),
+                "birth_date"=> null,
+                "state"=> strtoupper(trim($data['state'])),
+                "city"=> trim($data['city']),
+                "zip"=> trim($data['zipCode']),
+                "sms_consent"=>  (boolean)($data['optIn'] ?? null || False ?: False),
+                "experience"=> $data['experience'],
+                "accidents"=> $data['accidents'],
+                "violations"=> $data['violations'],
+                "source"=> 5, // 5 - JobsInTrucks
+            ],
+            'id' => $form->id,
+            'name' => $form->title,
+            'submission' => $submission->id,
+            'returnUrl' => $form->getRedirectUrl(),
+            'AppReferrer' => trim($data['referrerValue']),
+            'CompanyName' => trim($data['companyName']),
+            'CompanyId' => trim($data['atsCompanyId']),
+            'DriverId' => $driverId,
         ];
 
-        // Data has already been assigned
-        $used = [
-            'companyName','atsCompanyId','source','sourceId','referrerValue',
-            'firstName','lastName',
-            'city','state','zipCode',
-            'email','cellPhone','optIn',
-        ];
+        if (!empty($uploads)){
+            $json['uploads'] = $uploads;
+        }
+
+        $usedFields = $requiredFields;
 
         // Loop through form data
         foreach ($data as $handle => $value) {
-
             // If data point was not used, add to JSON data
-            if (!in_array($handle, $used)) {
+            if (!in_array($handle, $usedFields)) {
                 $label = ($labels[$handle] ?? $handle);
                 $json[$label] = $value;
             }
-
         }
 
         // Return JSON data
